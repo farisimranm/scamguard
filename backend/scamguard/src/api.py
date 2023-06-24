b8f320fa141c339b5cd4721384efa4984df5a97a
Faris Imran <faris.ude@gmail.com>
2023-06-24 16:52:49 +0800

    Optimized backend

--- backend/scamguard/src/api.py
+++ backend/scamguard/src/api.py
@@ -3,35 +3,34 @@ from flask_cors import CORS
 import speech_recognition as sr
 from pydub import AudioSegment
 from os.path import splitext
+import tempfile
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'])
 
-def mp3_to_flac(mp3_path):
-    flac_path = "%s.flac" % splitext(mp3_path)[0]
-    song = AudioSegment.from_file(mp3_path)
-    song.export(flac_path, format = "flac")
-    return flac_path
-
-# TODO - ADD SUPPORT FOR OTHER AUDIO FORMATS
 @app.route('/api/speech-to-text', methods=['POST'])
 def speech_to_text():
     if 'file' not in request.files:
         return 'No file found in the request.'
     
     file = request.files['file']
-    file_name = file.filename
-    file.save(file_name)
-
-    # convert mp3 to flac to feed into SpeechRecognition
-    flac_path = mp3_to_flac(file_name)
-
-    # Perform speech-to-text processing using the SpeechRecognition library
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(flac_path) as source:
-        audio_data = recognizer.record(source)
-
+    filename = file.filename
+    print(f'Received {filename}')
+    
     try:
+        # convert audio file to flac to feed into SpeechRecognition
+        print(f'Processing {filename}...')
+        audio = AudioSegment.from_file(file)
+        temp_file = tempfile.NamedTemporaryFile(suffix=".flac", delete=False)
+        audio.export(temp_file.name, format="flac")
+        temp_file_path = temp_file.name
+        
+        # Perform speech-to-text processing using the SpeechRecognition library
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(temp_file_path) as source:
+            audio_data = recognizer.record(source)
+        
+        print(f'Running speech recognition...')
         text = recognizer.recognize_google(audio_data)
         return jsonify({'text': text}), 200
 
@@ -40,6 +39,9 @@ def speech_to_text():
 
     except sr.RequestError:
         return jsonify({'error': 'Error occurred while processing the audio.'}), 500
+    
+    except Exception as e:
+        return jsonify({'error': 'Error occurred during audio file processing: ' + str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
